da83941ce3e3b28594cab2ad82403b17b024cf39
asdfkco <84617097+asdfkco@users.noreply.github.com>
2022-06-14 22:41:26 +0900

    Initial commit

---
+++ study_naverLogin.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome('./chromedriver.exe')
+#암묵적으로 웹 자원 로드를 위해 3초까지 기다려 준다.
+time.sleep(3)
+
+driver.get("https://naver.com")
+time.sleep(2)
+driver.find_element(By.CLASS_NAME,'link_login').click()
+
+time.sleep(5)
+driver.find_element(By.CSS_SELECTOR,'#id').send_keys('rlacksdhr0630')
+driver.find_element(By.NAME,'pw').send_keys('rlacksdhr')
+driver.find_element(By.XPATH,'//*[@id="log.login"]').click()
---
+++ study_youtube.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome('./chromedriver.exe')
+
+driver.get("https://youtube.com")
+
+def search():
+    driver.find_element(By.XPATH, '//input[@id="search"]').send_keys('먹방')
+    driver.find_element(By.XPATH, '//*[@id="search-icon-legacy"]').click()
+
+
+search()
+
+
+def title():
+    time.sleep(3)
+    # 패스가 애매할뗀 fullxpath로
+    for i in range(1,13):
+    # ytd-section-list-renderer/div[2]/ytd-item-section-renderer[1]
+        title = driver.find_element(By.XPATH, f'/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[{i}]/div[1]/div/div[1]/div/h3/a/yt-formatted-string')
+        print(title.text)
+title()
+print("코드끝")
