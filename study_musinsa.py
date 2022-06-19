e1ff366f3dc5e68d17ea29c6a7940e1eba7df912
asdfkco <84617097+asdfkco@users.noreply.github.com>
2022-06-20 00:01:22 +0900

    .

--- study_musinsa.py
+++ study_musinsa.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.common.keys import Keys
 import openpyxl
 
 driver = webdriver.Chrome('C:\chromedriver.exe')
@@ -8,11 +8,24 @@ wb = openpyxl.Workbook()
 ws = wb.active
 
 driver.get('https://www.musinsa.com/app/')
-time.sleep(3)
-
+driver.implicitly_wait(3)
+driver.set_window_size(1920,1080)
 # ul = driver.find_element(By.CLASS_NAME,'ranking_category')
+# ws.append(['제품명','조회수','','',''])
 for i in range(1,25):
-    li = driver.find_element(By.XPATH,f'//*[@id="ranking_goods_pager"]/li[{i}]')
-    print(li.text)
+    driver.find_element(By.XPATH,f'//*[@id="ranking_goods_pager"]/li[{i}]').click()
+    for J in range(1,15):
+        driver.find_element(By.XPATH,f'//*[@id="ranking_goods_pager"]/li[{i}]').click()
+        #한바퀴  돌고 입력키가 안먹는 오류생김
+        driver.find_element(By.XPATH, f'//*[@id="ranking_goods"]/div[{J}]').find_element(By.TAG_NAME,'a').send_keys(Keys.CONTROL,Keys.ENTER)
+        driver.switch_to.window(driver.window_handles[-1])
+        title = driver.find_element(By.CLASS_NAME,'product_title').find_element(By.TAG_NAME,'em')
+        views = driver.find_element(By.ID,'pageview_1m')
+        like = driver.find_element(By.CLASS_NAME,'prd_like_cnt')
+        defult_price = driver.find_element(By.CLASS_NAME,'price-del')
+        sale_price = driver.find_element(By.ID,'list_price')
+        print(title.text,views.text,like.text,defult_price.text,sale_price.text)
+        driver.close()
+        driver.switch_to.window(driver.window_handles[0])
 # print(ul.text)
 print("코드끝")
